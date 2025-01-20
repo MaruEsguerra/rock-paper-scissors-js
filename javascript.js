7d70b7411efec9c783d07dab22b71f88f9c741b5
Maru Esguerra <162283602+MaruEsguerra@users.noreply.github.com>
2025-01-20 16:40:10 +0800

    feat: Release 1.1.0
    Adds new function that takes user input.

--- javascript.js
+++ javascript.js
@@ -2,16 +2,29 @@
     // It has to randomly return one of the following strings: rock, paper, or scissors
         // You might want to use Math.random which returns a number that's greater than or equal to 1
 function getComputerChoice() {
-    let ComputerChoice = Math.floor(Math.random() * 100) + 1;
+    let computerChoice = Math.floor(Math.random() * 100) + 1;
 
-    if (ComputerChoice <= 33) {
+    if (computerChoice <= 33) {
         return "rock";
-    } else if (ComputerChoice >= 66) {
+    } else if (computerChoice >= 66) {
         return "scissors";
     } else {
         return "paper";
     }
 }
 
-let test_result = getComputerChoice();
-console.log(test_result);
+// Create a function called getHumanChoice
+    // Use prompt to return one of the valid choices: rock, paper, scissors.
+        // Potentially code defensively so that the human can only choose rock, paper, or scissors.
+function getHumanChoice() {
+    let humanChoice = prompt("Rock, paper, or scissors?").toLowerCase();
+    while (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") {
+        humanChoice = prompt("Please pick only among: rock, paper, or scissors.").toLowerCase();
+    }
+    return humanChoice;
+}
+
+let PC_result = getComputerChoice();
+let user_result = getHumanChoice();
+console.log(PC_result);
+console.log(user_result);
